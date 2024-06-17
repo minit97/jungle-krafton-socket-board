eaee7e2f61a57fa38df8f9c36dbff44f39b89e19
박현민 <dndb4902@naver.com>
2024-06-17 21:16:06 +0900

    # front template commit

--- krafton-react/src/App.js
+++ krafton-react/src/App.js
@@ -1,11 +1,17 @@
-import './App.css';
-import { Route, Routes } from 'react-router-dom';
+import {Route, Routes} from 'react-router-dom';
 
+import Home from './pages/Home'
+import SignIn from './pages/SignIn'
+import SignUp from './pages/SignUp'
+import Board from './pages/Board'
 
 function App() {
   return (
       <Routes>
-        {/*<Route path="/" element={<Home />} />*/}
+          <Route path="/" element={<Home />}/>
+          <Route path="/login" element={<SignIn />}/>
+          <Route path="/join" element={<SignUp />}/>
+          <Route path="/board" element={<Board />} />
       </Routes>
   );
 }
