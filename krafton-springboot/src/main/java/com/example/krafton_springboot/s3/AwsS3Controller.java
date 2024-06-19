debfcc9e52fcecb3172a0a18c8684175d5254ec6
박현민 <dndb4902@naver.com>
2024-06-19 15:56:39 +0900

    # AWS S3 + CloudFront

---
+++ krafton-springboot/src/main/java/com/example/krafton_springboot/s3/AwsS3Controller.java
@@ -0,0 +1,32 @@
+package com.example.krafton_springboot.s3;
+
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.web.bind.annotation.*;
+import org.springframework.web.multipart.MultipartFile;
+
+import java.io.IOException;
+
+@Slf4j
+@RequiredArgsConstructor
+@RequestMapping("/api/s3")
+@RestController
+public class AwsS3Controller {
+
+    private final AwsS3Service awsS3Service;
+    @GetMapping("/test")
+    public String test() {
+        return "ok";
+    }
+
+
+    @PostMapping("/resource")
+    public AwsS3 upload(@RequestPart("file") MultipartFile multipartFile) throws IOException {
+        return awsS3Service.upload(multipartFile,"upload");
+    }
+
+    @DeleteMapping("/resource")
+    public void remove(AwsS3 awsS3) {
+        awsS3Service.remove(awsS3);
+    }
+}
